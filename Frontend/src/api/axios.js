02023445f7c72de9482409c6bcc66da3081d8302
Ankur Kumar Sah <64032480+mitron8@users.noreply.github.com>
2026-04-08 21:41:24 +0530

    Update axios.js

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -2,6 +2,6 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:"https://veloura-e-commerce-backend.onrender.com" 
+    baseURL:"https://veloura-e-commerce-backend.onrender.com/api" 
 })
 export default api;
